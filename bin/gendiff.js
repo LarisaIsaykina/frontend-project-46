54aef1a44e0acdb74af52c30b030ad20ba87cc15
Larisa <isaykinalarisa@gmail.com>
2022-10-13 19:23:44 -0400

    edited gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+const { program } = require('commander');
 
-const program = new Command();
-program.option('-h, --help', 'display help for command')
+program
+.name("gendiff ")
+.usage("[options]")
 .description( 'Compares two configuration files and shows a difference.')
 .version('0.0.1', '-V, --version', 'output the version number')
 .parse();
