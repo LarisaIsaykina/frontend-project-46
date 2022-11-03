3c1ea93bd9f14ef01b2f3c153567957d951da839
Larisa <isaykinalarisa@gmail.com>
2022-11-03 16:59:27 -0400

    edited stylish.js

--- treeGenerator.js
+++ treeGenerator.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 
     const generateTree = (fileContent1, fileContent2) => {
-      const getDifference = (array1, array2, name) => {
+      const iter = (array1, array2, name) => {
         if (!_.has(array1, name)) return { name, state: 'added', value: array2[name] };
         if (!_.has(array2, name)) return { name, state: 'deleted', value: array1[name] };
         if (_.isObject(array1[name]) && _.isObject(array2[name])) {
@@ -17,6 +17,6 @@ import _ from 'lodash';
       };
     
       const keys = _.sortBy(_.union(Object.keys(fileContent1), Object.keys(fileContent2)));
-      return keys.map((key) => getDifference(fileContent1, fileContent2, key));
+      return keys.map((key) => iter(fileContent1, fileContent2, key));
     };
     export default generateTree;
