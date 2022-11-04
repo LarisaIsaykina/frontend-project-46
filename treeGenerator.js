90fbe59f0fb3b3d17e1058cea1c976aa5f77fdd3
Larisa <isaykinalarisa@gmail.com>
2022-11-04 10:52:13 -0400

    corrected linter

--- treeGenerator.js
+++ treeGenerator.js
@@ -7,16 +7,15 @@ const generateTree = (fileContent1, fileContent2) => {
     if (!_.has(array2, name)) return { name, state: 'deleted', value: array1[name] };
     if (_.isObject(array1[name]) && _.isObject(array2[name])) {
       return { name, state: 'nested', children: generateTree(array1[name], array2[name]) };
-        }
-      if (array1[name] !== array2[name]) {
-        return { name, state: 'changed', previousValue: array1[name], currentValue: array2[name],
-          };
-        }
+    }
+    if (array1[name] !== array2[name]) {
+      return { name, state: 'changed', previousValue: array1[name], currentValue: array2[name],
+      };
+    }
     return { name, state: 'unchanged', value: array1[name] };
   };
   const keys = _.sortBy(_.union(Object.keys(fileContent1), Object.keys(fileContent2)));
-    return keys.map((key) => iter(fileContent1, fileContent2, key));
-  };
+  return keys.map((key) => iter(fileContent1, fileContent2, key));
+};
 
-  export default generateTree;
-  
+export default generateTree;
