a52b8ca3dd306c6d99cd2c6eb15aa5e52e591070
Larisa <isaykinalarisa@gmail.com>
2022-11-04 11:30:21 -0400

    corrected linter

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,12 +1,12 @@
 import { test, expect } from '@jest/globals';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import { join, dirname } from 'path';
 import genDiff from '../factory.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
 const getFileContent = (fileName) => readFileSync(getFixturePath(fileName), 'utf-8');
 const absolutePath1 = getFixturePath('file1.json');
